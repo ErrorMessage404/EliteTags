9105f832f928ea3da09dad2c99e1ffb6450740ea
ErrorMessage404logins@gmail.com <errormessage404logins@gmail.com>
2021-02-21 16:11:24 -0500

    Progressed on Tags Menu

---
+++ src/main/java/me/error/tags/MenuSystem/menu/TagsMenu.java
@@ -0,0 +1,27 @@
+package me.error.tags.MenuSystem.menu;
+
+import me.error.tags.EliteTags;
+import me.error.tags.MenuSystem.Menu;
+import me.error.tags.Utils.BasicUtils;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class TagsMenu extends Menu {
+
+    private static EliteTags plugin;
+
+    public String getMenuName() {
+        return BasicUtils.chat(plugin.getConfig().getString("Customization.TagsMenu.MenuTitle"));
+    }
+
+    public int getSlots() {
+        return plugin.getConfig().getInt("Customization.TagsMenu.MenuSize");
+    }
+
+    public void handleMenu(InventoryClickEvent e) {
+
+    }
+
+    public void setMenuItems() {
+
+    }
+}
---
+++ src/main/java/me/error/tags/Utils/ItemUtils.java
@@ -0,0 +1,13 @@
+package me.error.tags.Utils;
+
+import me.error.tags.EliteTags;
+import org.bukkit.inventory.ItemStack;
+
+public class ItemUtils {
+
+    private EliteTags plugin;
+
+    public static ItemStack TagItem(String name, String formatedName, String description, Boolean requiresPermission, String permission) {
+
+    }
+}
